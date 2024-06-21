957a6cdc7200fcb8f82714e3960e3d61a365d8fe
Raj Manish <13066821+rajmanish23@users.noreply.github.com>
2024-06-21 12:51:24 +0530

    done with the component. hopefully...

--- src/components/ProtectedRoute/index.jsx
+++ src/components/ProtectedRoute/index.jsx
@@ -1,1 +1,12 @@
-import {Router} from 'react-router-dom'
+import {Route, Redirect} from 'react-router-dom'
+import Cookie from 'js-cookie'
+
+const ProtectedRoute = props => {
+  const token = Cookie.get('jwt_token')
+  if (token === undefined) {
+    return <Redirect to="/login" />
+  }
+  return <Route {...props} />
+}
+
+export default ProtectedRoute
