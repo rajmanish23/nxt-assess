5c8aca3984b26feee9c9e696fd2a9444afa3a0d7
Raj Manish <13066821+rajmanish23@users.noreply.github.com>
2024-06-24 15:31:24 +0530

    Added JSX for header common component, and made an empty css file

---
+++ src/components/Header/index.jsx
@@ -0,0 +1,29 @@
+import {Link, withRouter} from 'react-router-dom'
+import Cookie from 'js-cookie'
+
+import './index.css'
+
+import logo from '../../assets/header-logo.png'
+
+const Header = props => {
+  const onClickLogout = () => {
+    Cookie.remove('jwt_token')
+    const {history} = props
+    history.replace('/login')
+  }
+
+  return (
+    <nav className="header-container">
+      <img src={logo} alt="website logo" className="header-logo-img" />
+      <button
+        type="button"
+        onClick={onClickLogout}
+        className="header-logout-btn"
+      >
+        Logout
+      </button>
+    </nav>
+  )
+}
+
+export default withRouter(Header)
