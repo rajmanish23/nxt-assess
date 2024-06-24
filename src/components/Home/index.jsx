b512d5a852f4ec31b3e1e720161d871222e34378
Raj Manish <13066821+rajmanish23@users.noreply.github.com>
2024-06-24 18:15:49 +0530

    jsx code of home route, styling yet to be done

--- src/components/Home/index.jsx
+++ src/components/Home/index.jsx
@@ -1,10 +1,66 @@
 import Header from '../Header'
 
-const Home = () => (
-  <>
-    <Header />
-    <h1>Home</h1>
-  </>
-)
+import './index.css'
+
+import image from '../../assets/home-image.png'
+
+const Home = props => {
+  const onCLickOpenAssessment = () => {
+    const {history} = props
+    history.replace('/assessment')
+  }
+
+  const renderInstructionsCard = () => (
+    <div className="home-instructions-card">
+      <h1 className="home-instructions-heading">Instructions</h1>
+      <ul className="home-instructions-list-container">
+        <li className="home-instructions-list-item">
+          <span className="home-instructions-list-item-main-text">
+            Total Questions
+          </span>
+          : 10
+        </li>
+        <li>
+          <span className="home-instructions-list-item-main-text">
+            Types of Questions
+          </span>
+          : MCQs
+        </li>
+        <li>
+          <span className="home-instructions-list-item-main-text">
+            Duration
+          </span>
+          : 10 Mins
+        </li>
+        <li>
+          <span className="home-instructions-list-item-main-text">
+            Marking Scheme
+          </span>
+          : Every Correct response, get 1 mark
+        </li>
+        <li>
+          All the progress will be lost, if you reload during the assessment
+        </li>
+      </ul>
+      <button
+        type="button"
+        className="home-instructions-button"
+        onClick={onCLickOpenAssessment}
+      >
+        Start Assessment
+      </button>
+    </div>
+  )
+
+  return (
+    <div className="global-bg-container">
+      <Header />
+      <div className="home-bg-container">
+        <img className="home-image" src={image} alt="assessment" />
+        {renderInstructionsCard()}
+      </div>
+    </div>
+  )
+}
 
 export default Home
