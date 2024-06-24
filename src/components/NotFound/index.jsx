469c9e4bf74005dd8c2c8a96ab48a2d1a75b1409
Raj Manish <13066821+rajmanish23@users.noreply.github.com>
2024-06-24 15:16:03 +0530

    Added Not Found/404 page

--- src/components/NotFound/index.jsx
+++ src/components/NotFound/index.jsx
@@ -1,3 +1,17 @@
-const NotFound = () => <h1>Not Found</h1>
+import './index.css'
+
+import notFoundImg from '../../assets/not-found-image.png'
+
+const NotFound = () => (
+  <div className="not-found-bg-container">
+    <img className="not-found-image" src={notFoundImg} alt="not found" />
+    <div className="not-found-text-container">
+      <h1 className="not-found-heading">Page Not Found</h1>
+      <p className="not-found-para">
+        We are sorry, the page you requested could not be found
+      </p>
+    </div>
+  </div>
+)
 
 export default NotFound
