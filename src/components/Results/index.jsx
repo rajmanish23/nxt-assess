fd1485b62fafd84544b44f7bf53636794fdaf64f
Raj Manish <13066821+rajmanish23@users.noreply.github.com>
2024-06-24 19:01:04 +0530

    started work on results page

--- src/components/Results/index.jsx
+++ src/components/Results/index.jsx
@@ -1,3 +1,23 @@
-const Results = () => <h1>Results</h1>
+import Header from '../Header'
+
+import './index.css'
+
+import inTimeImage from '../../assets/results-in-time-image.png'
+import notInTimeImage from '../../assets/results-not-in-time-image.png'
+
+const Results = ({time = 0, score = 0, history}) => {
+  const onClickOpenAssessment = () => {
+    history.push('/assessment')
+  }
+
+  const imageToUse = time === 0 ? notInTimeImage : inTimeImage
+
+  return (
+    <div className="global-bg-container">
+      <Header />
+      <div></div>
+    </div>
+  )
+}
 
 export default Results
