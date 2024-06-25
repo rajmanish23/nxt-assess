cd59c8d4cf627fd980d9417ae34a41bb4e79441e
Raj Manish <13066821+rajmanish23@users.noreply.github.com>
2024-06-25 15:09:43 +0530

    Wrote JSX for results page

--- src/components/Results/index.jsx
+++ src/components/Results/index.jsx
@@ -11,11 +11,54 @@ const Results = ({time = 0, score = 0, history}) => {
   }
 
   const imageToUse = time === 0 ? notInTimeImage : inTimeImage
+  const imageAltToUse = time === 0 ? 'time up' : 'submit'
+
+  const date = new Date(0)
+  date.setSeconds(time)
+  const timeString = date.toISOString().substring(11, 19)
 
   return (
     <div className="global-bg-container">
       <Header />
-      <div></div>
+      <div className="results-bg-container">
+        <div className="results-card-container">
+          <img className="results-image" src={imageToUse} alt={imageAltToUse} />
+          {
+            // Heading in card
+            time !== 0 ? (
+              <h1 className="results-score-heading">
+                Congrats! You completed the assessment.
+              </h1>
+            ) : (
+              <h1 className="results-time-heading">Time is up</h1>
+            )
+          }
+          {
+            // Paragraph/text thing in the card
+            time !== 0 ? (
+              <p className="results-score-para">
+                Time Taken:
+                <span className="results-final-time-display">{timeString}</span>
+              </p>
+            ) : (
+              <p className="results-time-para">
+                You did not complete the assessment within the time.
+              </p>
+            )
+          }
+          <p className="results-final-score-text">
+            Your Score:
+            <span className="results-final-score-number">{score}</span>
+          </p>
+          <button
+            className="results-restart-button"
+            onClick={onClickOpenAssessment}
+            type="button"
+          >
+            Reattempt
+          </button>
+        </div>
+      </div>
     </div>
   )
 }
