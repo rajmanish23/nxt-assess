11f33cbf324a3543f7252ab5fb935ee25737c7ac
Raj Manish <13066821+rajmanish23@users.noreply.github.com>
2024-06-26 20:31:55 +0530

    implemented JSX for multi select type questions with text as options

---
+++ src/components/Assessment/sub_components/Question/TextMultiSelect/index.jsx
@@ -0,0 +1,40 @@
+const TextMultiSelect = props => {
+  const {
+    optionDetails,
+    setScoreFunc,
+    isSelected,
+    optionIndex,
+    setActiveOptionId,
+  } = props
+
+  const {text, isCorrect} = optionDetails
+
+  const selectedClass = isSelected ? 'active-option-highlight' : ''
+
+  const checkCorrectAndSetScore = () => {
+    if (isCorrect) {
+      setScoreFunc(1)
+    } else {
+      setScoreFunc(0)
+    }
+  }
+
+  const onClickSetAsActive = () => {
+    checkCorrectAndSetScore()
+    setActiveOptionId(optionIndex)
+  }
+
+  return (
+    <li className="question-text-option-container">
+      <button
+        className={`question-text-option-button ${selectedClass}`}
+        onClick={onClickSetAsActive}
+        type="button"
+      >
+        {text}
+      </button>
+    </li>
+  )
+}
+
+export default TextMultiSelect
