4f58ebd1e1c9a0c356f5c66d994270dcfc83ac9c
Raj Manish <13066821+rajmanish23@users.noreply.github.com>
2024-06-26 18:59:47 +0530

    added render controller method to render questions based on type of options

--- src/components/Assessment/sub_components/Question/index.jsx
+++ src/components/Assessment/sub_components/Question/index.jsx
@@ -1,3 +1,9 @@
+import {useState} from 'react'
+
+import DropdownSingleSelect from './DropdownSingleSelect'
+import ImageMultiSelect from './ImageMultiSelect'
+import TextMultiSelect from './TextMultiSelect'
+
 const Question = props => {
   const {
     nextQuestionFunction,
@@ -14,9 +20,50 @@ const Question = props => {
     </button>
   )
 
+  const renderQuestion = () => {
+    const {questionText, optionsType, options} = questionData
+
+    let optionsView = null
+    switch (optionsType) {
+      case 'DEFAULT':
+        optionsView = (
+          <ul className="question-options-list-container">
+            {options.map(eachOption => (
+              <TextMultiSelect optionDetails={eachOption} />
+            ))}
+          </ul>
+        )
+        break
+      case 'IMAGE':
+        optionsView = (
+          <ul className="question-options-list-container">
+            {options.map(eachOption => (
+              <ImageMultiSelect optionDetails={eachOption} />
+            ))}
+          </ul>
+        )
+        break
+      case 'SINGLE_SELECT':
+        optionsView = <DropdownSingleSelect optionDetails={options} />
+        break
+      default:
+        optionsView = null
+        break
+    }
+
+    return (
+      <div className="question-container">
+        <p className="question-main-text">
+          {`${currentQuestionIndex + 1}. ${questionText}`}
+        </p>
+        {optionsView}
+      </div>
+    )
+  }
+
   return (
     <div className="question-bg-container">
-      <h1>{`Question Number: ${currentQuestionIndex + 1}`}</h1>
+      {renderQuestion()}
       {isNextQuestionAvailable && renderNextQuestionButton()}
     </div>
   )
