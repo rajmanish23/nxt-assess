e84798b6d78a4440df3d6620855778b2cb14a016
Raj Manish <13066821+rajmanish23@users.noreply.github.com>
2024-06-26 20:32:18 +0530

    implemented JSX for multi select type questions with images as options

---
+++ src/components/Assessment/sub_components/Question/ImageMultiSelect/index.jsx
@@ -0,0 +1,40 @@
+const ImageMultiSelect = props => {
+  const {
+    optionDetails,
+    setScoreFunc,
+    isSelected,
+    optionIndex,
+    setActiveOptionId,
+  } = props
+
+  const {text, imageUrl, isCorrect} = optionDetails
+
+  const selectedClass = isSelected ? 'active-option-outline' : ''
+
+  const checkCorrectAndSetScore = () => {
+    if (isCorrect) {
+      setScoreFunc(1)
+    } else {
+      setScoreFunc(0)
+    }
+  }
+
+  const onClickSetAsActive = () => {
+    checkCorrectAndSetScore()
+    setActiveOptionId(optionIndex)
+  }
+
+  return (
+    <li className="question-image-option-container">
+      <button
+        className={`question-image-option-button ${selectedClass}`}
+        onClick={onClickSetAsActive}
+        type="button"
+      >
+        <img src={imageUrl} alt={text} className="question-image" />
+      </button>
+    </li>
+  )
+}
+
+export default ImageMultiSelect
