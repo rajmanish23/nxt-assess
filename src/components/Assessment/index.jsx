0a0d3e1de02fdaed6734be90b399170c128e1e37
Raj Manish <13066821+rajmanish23@users.noreply.github.com>
2024-07-03 19:10:28 +0530

    centered stuff properly

--- src/components/Assessment/index.jsx
+++ src/components/Assessment/index.jsx
@@ -124,7 +124,7 @@ const Assessment = ({history}) => {
   }
 
   const renderLoader = () => (
-    <div data-testid="loader">
+    <div className="loader-container" data-testid="loader">
       <Loader type="ThreeDots" color="#263868" height={50} width={50} />
     </div>
   )
